80ea72173316b286615f6a458688cd50668072f8
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2016-10-26 17:14:29 -0700

    add lesson6

---
+++ lessons/lesson6/find-friend-sanctuary/S.js
@@ -0,0 +1,6 @@
+var sanctuary = require('sanctuary');
+var create = sanctuary.create;
+var env = sanctuary.env;
+var S = create({ checkTypes: true, env: env });
+module.exports = S;
+
---
+++ lessons/lesson6/find-friend-sanctuary/index.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+var F = require('./friend');
+var R = require('ramda');
+
+// parseUserId :: String -> Maybe UserId
+// findBestFriendshipByUserId :: UserId -> Maybe Friendship
+// selectFriendId :: Friendship -> UserId
+// findUserById :: UserId -> Maybe User
+// selectUserName :: User -> String
+
+// findBestFriendNameByUserId :: String -> Maybe String
+var findBestFriendNameByUserId = R.pipe(
+  F.parseUserId, // -> Maybe UserId
+  R.curry(R.chain)(F.findBestFriendshipByUserId), // -> Maybe Friendship
+  R.curry(R.map)(F.selectFriendId), // -> Maybe UserId
+  R.curry(R.chain)(F.findUserById), // -> Maybe User
+  R.curry(R.map)(F.selectUserName) // -> Maybe String
+);
+
+assert.deepEqual(findBestFriendNameByUserId('1'), { isNothing: false, isJust: true, value: 'u2' });
+assert.deepEqual(findBestFriendNameByUserId('sdf'), { isNothing: true, isJust: false });
+assert.deepEqual(findBestFriendNameByUserId('10'), { isNothing: true, isJust: false });
+assert.deepEqual(findBestFriendNameByUserId('3'), { isNothing: true, isJust: false });
+assert.deepEqual(findBestFriendNameByUserId('-1'), { isNothing: true, isJust: false });
